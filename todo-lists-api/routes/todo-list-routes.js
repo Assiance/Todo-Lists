172f51680693aafa1b061024886ad6af79fc42fb
Eight Ball <ewindham23@yahoo.com>
2024-04-06 15:05:47 -0400

    post update

--- todo-lists-api/routes/todo-list-routes.js
+++ todo-lists-api/routes/todo-list-routes.js
@@ -12,4 +12,25 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    try {
+        if (!req.body.title) {
+            res.status(400).send('Title must have a value.')
+            return;
+        }
+        if (!req.body.description) {
+            res.status(400).send('Description must have a value.')
+            return;
+        }
+        let newList = await TodoLists.create({
+            title: req.body.title,
+            description: req.body.description
+        });
+        res.status(201).send(newList);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(`Internal Server Error ${error}`)
+    }
+})
+
 module.exports = router;
--- todo-lists/src/pages/new-todo-list/NewTodoListPage.js
+++ todo-lists/src/pages/new-todo-list/NewTodoListPage.js
@@ -1,9 +1,56 @@
+import { useState } from "react";
+import TodoList from "../../components/todo-list/TodoList";
+
 function NewTodoListPage() {
+  const [data, setData] = useState({
+    title: '',
+    description: ''
+  });
+  //const [description, setDescription] = useState('');
+
   return (
     <div>
-      <h1>New Todo List</h1>
+
+      <div>
+        <h1>New Todo List</h1>
+      </div>
+
+      <div>
+        <label>Title</label>
+        <input type="text" placeholder="Enter Todolist title" value={data.title} onChange={(e) => { setData(e.target.value) }}></input>
+      </div>
+
+      <div>
+        <label>Description</label>
+        <input type="text" placeholder="Enter Todolist description" value={data.description} onChange={(e) => { setData(e.target.value) }}></input>
+      </div>
+
+      <div>
+        <button type="submit" className="btn btn-primary" onClick={postTodos}>Submit List</button>
+      </div>
+
     </div>
   )
+
+  function postTodos() {
+    const asyncPostTodos = async () => {
+      const url = `http://localhost:4000/todos`;
+
+      const options = {
+        method: 'POST'
+      }
+
+      try {
+        const response = await fetch(url, options);
+        const data = await response.json()
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+      setData(data);
+    }
+    asyncPostTodos();
+  }
 }
 
 export default NewTodoListPage;
